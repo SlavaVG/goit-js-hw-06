f9d419046d2483cb63f19d5ea07366a9616faab8
SlavaVG <shlikhten@gmail.com>
2021-12-29 19:28:16 +0200

    2

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,2 @@
-console.log(document);
-
-const body = document.body;
-console.log(body);
-
-const list = body.firstElementChild;
-console.log(list);
-
-const firstListItem = list.firstElementChild;
-console.log(firstListItem);
-
-const listItems = list.children;
-console.log(listItems);
+const menuItemsByTagName = document.querySelectorAll("h2");
+console.log(`Number of categories:`, menuItemsByTagName.length);
