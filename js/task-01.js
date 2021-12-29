bbff2c6a9744dab2e7735f935db2c6d87115a0d7
SlavaVG <shlikhten@gmail.com>
2021-12-30 00:48:18 +0200

    Z1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
 const menuItemsByTagName = document.querySelectorAll("h2");
 console.log(`Number of categories:`, menuItemsByTagName.length);
+
+const meNu = document.querySelector("ul");
+for (let elem of meNu.children) {
+    const meNu4ul = elem.querySelector("h2");
+    console.log(`Category:`, meNu4ul.textContent);
+    const meNu5li = elem.querySelectorAll("li");
+    console.log(`Elements:`, meNu5li.length);
+    }
