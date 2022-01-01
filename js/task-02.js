7ef80c9a61377e573c3ce11e449c4a93af157df7
SlavaVG <shlikhten@gmail.com>
2022-01-01 23:18:41 +0200

    z2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const list = document.querySelector("#ingredients");
+  const lastItem = document.createElement("li");
+  lastItem.textContent = ingredients[i];
+  list.append(lastItem);
+}
