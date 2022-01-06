eaf61d1f30449736c62df07165e9514be0406da8
SlavaVG <shlikhten@gmail.com>
2022-01-06 10:40:40 +0200

    Hw4

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+const buttonDec = document.querySelector("[data-action=decrement]")
+const buttonInc = document.querySelector("[data-action=increment]")
+
+buttonDec.addEventListener('click', deButtonClick)
+buttonInc.addEventListener('click', inButtonClick)
+
+function deButtonClick() {
+    counterValue.textContent--
+}
+function inButtonClick() {
+    counterValue.textContent++
+}
+const counterValue = document.querySelector('span')
+console.log(counterValue.textContent)
