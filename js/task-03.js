0ed201579bd77165d84904b75d35df8b8b136dcf
SlavaVG <shlikhten@gmail.com>
2022-01-06 12:08:48 +0200

    wh3 0601 1208

--- js/task-03.js
+++ js/task-03.js
@@ -11,46 +11,15 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-    {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-
 ];
 
 
-for (const image of images) {
-  const list = document.querySelector(".gallery");
-  list.id = 'grid';
-  const liEl = document.createElement("li");
-  liEl.classList.add('noList');
-  const imgEl = document.createElement("img");
-  imgEl.classList.add('gallery__img');
-  imgEl.src = image.url;
-  imgEl.alt = image.alt;
-  liEl.appendChild(imgEl);
-  list.appendChild(liEl);
-  // liEl.insertAdjacentHTML('beforeend', imgEl);
-  // list.insertAdjacentHTML('beforeend', liEl);
+const liImages = images
+  .map(pic => `<li><img src=${pic.url} alt=${pic.alt} width='720px'></li>`)
+  .join('');
 
-}
+const ulImages = document.querySelector('.gallery');
+ulImages.style.listStyleType = 'none';
+ulImages.style.display = 'flex';
+ulImages.style.flexDirection = 'column';
+ulImages.insertAdjacentHTML('beforeend', liImages);
