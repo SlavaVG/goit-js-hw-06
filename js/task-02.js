b4479b27d3ac6fdcd3c752afd1f8f413b4f109a9
SlavaVG <shlikhten@gmail.com>
2022-01-18 15:21:39 +0200

    33

--- js/task-02.js
+++ js/task-02.js
@@ -7,20 +7,10 @@ const ingredients = [
   'Condiments',
 ];
 
-for (let i = 0; i < ingredients.length; i += 1) {
-  const list = document.querySelector("#ingredients");
-  const lastItem = document.createElement("li");
-  lastItem.textContent = ingredients[i];
-  list.append(lastItem);
-}
-
-const list = document.querySelector("#ingredients");
-list.append('================================');
-
-var result = ingredients.reduce(function (sum, current, index, arr) {
-    const buoy = document.querySelector("#ingredients"); // присвоили постоянной значение узла (нашли елемент)
-    const lastBuoy = document.createElement("li"); // присвоили постоянной значение узла  в дереве разметки 
-    // lastBuoy.textContent = ingredients[index]; // (так работает) присвоили постоянной текстовое содержимое елемента согласное его номера в массиве
-    lastBuoy.textContent = current; // (и так работает!) присвоили постоянной текстовое содержимое елемента
-    buoy.append(lastBuoy); // добавили постоянную в конец после найденного выше узла. (создали дочерний елемент/узел в дереве разметки)
-  }, 0);
+const listEl = document.querySelector('ul#ingredients');
+let itemArr = ingredients.map(currentValue => {
+  const newElRef = document.createElement('li');
+  newElRef.textContent = currentValue;
+  return newElRef;
+});
+  listEl.append(...itemArr);
